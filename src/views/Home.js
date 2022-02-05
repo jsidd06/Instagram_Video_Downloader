6a90e022c106676f4c63c75b1c4810f2902754ea
siddharth <jsidd06@gmail.com>
2022-02-05 16:12:02 +0530

    set a api with result

--- src/views/Home.js
+++ src/views/Home.js
@@ -2,19 +2,8 @@ import axios from 'axios';
 import React, { useState } from 'react';
 
 function Home() {
-  const [downloadLink, setDownloadLink] = useState("");
+  const [downloadLink, setDownloadLink] = useState('');
   const [downloadLink2, setDownloadLink2] = useState("");
-
-  // var options = {
-  //   method: "GET",
-  //   url: `https://murbil-instagram-media-download-v1.p.rapidapi.com/GetVideoLink`,
-  //   params: { Url: downloadLink2 }
-  //   headers: {
-  //     "x-rapidapi-host": "murbil-instagram-media-download-v1.p.rapidapi.com",
-  //     "x-rapidapi-key": "fe414cc81emshafef0d1b1c855cbp17df7djsn4a21f768c675",
-  //   },
-  // };
-
  var options = {
    method: "GET",
    url: "https://indianrailways.p.rapidapi.com/findstations.php",
@@ -27,7 +16,42 @@ function Home() {
 
   const submitLinkHandler = (event) => {
     event.preventDefault();
-    // axios
+    axios
+      .request(options)
+      .then(function (response) {
+        console.log(response);
+        setDownloadLink(response.data);
+      })
+      .catch(function (error) {
+        console.error(error);
+      });
+  };
+  return (
+    <div>
+      <h1>Home</h1>
+      <input onChange={(e) => setDownloadLink2(e.target.value)} />
+      <button onClick={submitLinkHandler}>Check now</button>
+      {downloadLink && (
+        <>
+          <div>
+            {console.log(downloadLink)}
+            {downloadLink.stations.map((s) => (
+              <>
+                <h1>{s.stationName}</h1>
+                <h1>{s.stationCode}</h1>
+              </>
+            ))}
+          </div>
+        </>
+      )}
+    </div>
+  );
+}
+
+export default Home;
+
+
+// axios
     //   .get(
     //     "https://livescore6.p.rapidapi.com/matches/v2/list-live? Category=" +
     //       downloadLink2 + 
@@ -46,28 +70,3 @@ function Home() {
     //   .catch((error) => {
     //     console.log(error);
     //   });
-    axios
-      .request(options)
-      .then(function (response) {
-        console.log(response);
-        setDownloadLink(response.data);
-      })
-      .catch(function (error) {
-        console.error(error);
-      });
-  };
-  return (
-    <div>
-      <h1>Home</h1>
-      <input onChange={(e) => setDownloadLink2(e.target.value)} />
-      <button onClick={submitLinkHandler}>Check now</button>
-      {downloadLink && (
-        <div>
-          <h1>{downloadLink.stations}</h1>
-        </div>
-      )}
-    </div>
-  );
-}
-
-export default Home;
